af4405139be0f5618beb305de3ec7c4d292a4577
rakesh.u <rakesh.u@pthinks.com>
2024-09-03 18:59:39 +0530

    After commiting third time

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World From Rakesh U!" << std::endl;
+    std::cout << "Hello, World From Rakesh U! From trigger" << std::endl;
     return 0;
 }
 
